7c14d6a150cca13a81836afd577d9777cebc2caf
Raphael Boateng-Amponsah (Kainos) <raphael.boateng-amponsah@defra.onmicrosoft.com>
2023-02-17 13:36:33 +0000

    heartbeat and exception error

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -2,11 +2,21 @@ import pika, json
 
 params = pika.URLParameters('amqps://yxkzlipj:ewKx_gGURTsQdGe_qWUxp1pEEI9Z8Njj@fly.rmq.cloudamqp.com/yxkzlipj')
 
+
+params.heartbeat = 240
+
 connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-
 def publish(method, body):
-    properties = pika.BasicProperties(method)
-    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
+    try:
+        properties = pika.BasicProperties(method)
+        channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
+        print("Message published successfully!")
+    except (pika.exceptions.AMQPError, pika.exceptions.ChannelWrongStateError) as e:
+        print(f"Error publishing message: {e}")
+    finally:
+        channel.close()
+        connection.close()
+
